7191764beaa6dd28a20321591dfbeda124ed66c0
USER <user@AL01425718.local>
2022-11-03 11:15:10 +0900

    Init

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+    "plugin:storybook/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  rules: {},
+};
